5702d65dcf505761768f9dad5218f2f772dc94dc
Anna Lopatina <anjuta377@gmail.com>
2023-02-19 16:41:07 +0300

    исправляет ошибки в функциях

--- js/functions.js
+++ js/functions.js
@@ -1,6 +1,6 @@
 // Функция для проверки длины строки
-const stringLength = (string, value) => string.length <= value;
-stringLength('проверяемая строка', 10);
+const isValidLength = (string, value) => string.length <= value;
+isValidLength('проверяемая строка', 10);
 
 // Функция для проверки, является ли строка палиндромом.
 const isPalindrome = (string) => {
@@ -14,24 +14,22 @@ isPalindrome('Лёша на полке клопа нашёл ');
 
 // Функция, которая принимает строку, извлекает содержащиеся в ней цифры от 0 до 9
 // и возвращает их в виде целого положительного числа.
-
-const number = (value) => {
+const extractNumbersFromString = (value) => {
   value = String(value);
-  let newNumber = '';
+  let result = '';
   for (let i = 0; i <= value.length; i++) {
     if (parseInt(value[i], 10) >= 0) {
-      newNumber += value[i];
+      result += value[i];
     }
   }
-  return parseInt(newNumber, 10);
+  return parseInt(result, 10);
 };
-number(1.5);
+extractNumbersFromString(1.5);
 
 // Функция, которая принимает три параметра: исходную строку, минимальную длину и строку с добавочными символами
 // — и возвращает исходную строку, дополненную указанными символами до заданной длины.
 // Символы добавляются в начало строки. Если исходная строка превышает заданную длину,
 // она не должна обрезаться. Если «добивка» слишком длинная, она обрезается с конца.
-
 const failAddress = (string, minLength, addString) => {
   let newString = '';
   while (newString.length + string.length < minLength) {
